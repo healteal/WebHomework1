c0e11b0f8162facfe7dab1f2229a8b65c3a72bf9
healteal <kirylshuppa@gmail.com>
2023-02-24 15:58:31 +0300

    Added second homework

--- src/main/java/General.java
+++ src/main/java/General.java
@@ -2,12 +2,18 @@ import models.Instructor;
 import models.Section;
 import models.SectionType;
 
+import java.sql.*;
 import java.util.HashSet;
 import java.util.Set;
 
 public class General {
     public static void main(String[] args) {
-        Set<Instructor> instructors = new HashSet<Instructor>();
+        //first_homework();
+        second_homework("jdbc:mysql://localhost:3306/gym", "root", "123qwe");
+    }
+
+    private static void first_homework() {
+        Set<Instructor> instructors = new HashSet<>();
         instructors.add(new Instructor("Петров Пётр Петрович", 35,
                 new Section("Дзюдо", 20, SectionType.first_hall),
                 5, "МС по дзюдо"));
@@ -27,4 +33,38 @@ public class General {
                         || instructor.getSection().getSectionType() == SectionType.first_hall).
                 forEach(System.out::println);
     }
+
+    private static void second_homework(String url, String username, String password) {
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            Statement statement = connection.createStatement();
+            statement.executeQuery("SELECT * FROM instructor WHERE (age > 30 AND (id_section = 1 OR id_section = 4));");
+            ResultSet resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("fio"));
+            }
+            statement.clearBatch();
+            statement.executeUpdate("INSERT INTO instructor(fio, age, id_section, experience, education) " +
+                    "VALUES ('Шуппо Кирилл Геннадьевич', 25, 1, 5, 'КМС по Дзюдо');");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM instructor WHERE fio LIKE ?");
+            preparedStatement.setString(1, "Шуппо%");
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id") + " "
+                        + resultSet.getString("fio") + " "
+                        + resultSet.getInt("age") + " "
+                        + resultSet.getString("experience") + " "
+                        + resultSet.getString("education"));
+            }
+            statement.clearBatch();
+            statement.executeUpdate("DELETE FROM instructor WHERE fio = 'Шуппо Кирилл Геннадьевич'");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
+
+
+
+
